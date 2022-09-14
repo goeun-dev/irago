061a6ed47e865c7d732fb7e122fe3afc97bb6c86
EGG <egg93>
2022-09-14 21:56:22 +0900

    feat: 공통VO 및 공통규칙 추가 - Sr 및 Idea 기능에서 공통으로 사용되는 UserVO 추가 - 커밋메시지규칙 추가

---
+++ src/main/java/com/gora/irago/domain/UserVO.java
@@ -0,0 +1,18 @@
+package com.gora.irago.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UserVO {
+
+    private String name;
+    private String auth;
+    private Date regDt;
+    private String delFl;
+    private Date delDt;
+    private String id;
+    private String pw;
+
+}
