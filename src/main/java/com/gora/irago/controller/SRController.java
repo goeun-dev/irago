6998fbd4f3575c4bc660df9b9f25ff778eb8c98c
EGG <egg93>
2022-09-27 17:12:08 +0900

    feat: SR기능 개발 중 - SR 목록 HTML 작업 중 - SRMapper: selectList() 수정

--- src/main/java/com/gora/irago/controller/SRController.java
+++ src/main/java/com/gora/irago/controller/SRController.java
@@ -1,7 +1,26 @@
 package com.gora.irago.controller;
 
+import com.gora.irago.domain.SRVO;
+import com.gora.irago.service.SRService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@RequestMapping("/sr")
 @Controller
 public class SRController {
+
+    @Autowired
+    SRService srService;
+
+    @RequestMapping("/list.do")
+    public String viewList(Model model){
+        List<SRVO> list = srService.selectList();
+        model.addAttribute("list", list);
+        return "/sr/srList.html";
+    }
 }
